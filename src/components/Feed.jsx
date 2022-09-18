05ff79340b633415fd68150c7823fcedf88063ca
Timi <alabitimi65@gmail.com>
2022-09-18 00:15:55 -0600

    fetched api from rapid api and added Video compnonet to render video data from api

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.item)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -13,7 +23,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography
           className="copyright"
@@ -22,6 +35,18 @@ const Feed = () => {
         >
           Copyright 2022 TiTube
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "#fff" }}
+        >
+          {selectedCategory} <span style={{ color: "#fc1503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={[videos]} />
       </Box>
     </Stack>
   );
