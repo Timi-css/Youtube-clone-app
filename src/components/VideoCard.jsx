e58c7abffde8211dab27f056c7493cd36d67fe51
Timi <alabitimi65@gmail.com>
2022-09-19 13:20:48 -0600

    added the VideoCard and ChannelCard components

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import {
+  demoThumbnailUrl,
+  demoVidoeUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  console.log(videoId, snippet);
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;
