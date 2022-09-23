3cfac8d78cc0ff80b3ebec40065aae49958eff07
Timi <alabitimi65@gmail.com>
2022-09-22 22:41:43 -0600

    passed the videos as props to the videoDetail page and added related videos

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -18,8 +18,8 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "#fff" }}>
-        Search results for:{" "}
-        <span style={{ color: "#fc1503" }}>{searchTerm}</span>videos
+        Search results for{" "}
+        <span style={{ color: "#fc1503" }}>{searchTerm}</span> videos
       </Typography>
 
       <Videos videos={videos} />
